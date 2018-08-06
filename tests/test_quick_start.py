99b51ae44db6168f2c8e5fbb59d22982367320a0
Aleksandr Seliverstov <seliverstov@ipavlov.ai>
2018-08-06 18:43:26 +0300

    remove ranking tests

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -101,8 +101,6 @@ PARAMS = {
         ("elmo/elmo_ru-news.json", "elmo_ru-news", ('IP',)): [ONE_ARGUMENT_INFER_CHECK],
     },
 
-    "ranking": {("ranking/ranking_insurance.json", "ranking", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
-                ("ranking/en_ranker_tfidf_wiki_test.json", "ranking", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK]},
     "squad": {
         ("squad/squad.json", "squad_model", ALL_MODES): [TWO_ARGUMENTS_INFER_CHECK],
         ("squad/squad_ru.json", "squad_model_ru", ALL_MODES): [TWO_ARGUMENTS_INFER_CHECK]
